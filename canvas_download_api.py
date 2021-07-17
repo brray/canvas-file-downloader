d07fa9f53638312a3c3f149f48a47242d79507e2
brray <71157246+brray@users.noreply.github.com>
2021-07-16 22:18:50 -0500

    Add Python code

---
+++ canvas_download_api.py
@@ -0,0 +1,64 @@
+import requests
+import os
+import urllib.request
+from pathlib import Path
+
+class Course:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name.replace(' ', '_').replace(':', '')
+        self.files = []
+        self.get_files()
+        os.mkdir(os.path.join(Path.home(), 'canvas_documents', self.name))
+
+    def get_files(self):
+
+        r = canvas.get(api_url + '/courses/' + str(self.id) + '/files?per_page=3000', headers=headers)
+
+        for file in r.json():
+            try:
+                self.files.append(File(file['id'], file['display_name'], file['filename'], file['url']))
+            except:
+                pass
+
+    def download_files(self):
+        for file in self.files:
+            file.download_file(self.name)
+
+class File:
+    def __init__(self, id, display_name, filename, url):
+        self.id = id
+        self.display_name = display_name
+        self.filename = filename
+        self.url = url
+
+    def download_file(self, folder):
+        print('call fdl')
+        myfile = canvas.get(self.url)
+
+        open(os.path.join(Path.home(), 'canvas_documents', folder, self.filename), 'wb').write(myfile.content)
+
+if __name__ == '__main__':
+    canvas = requests.Session()
+
+    api_url = 'https://mst.instructure.com/api/v1'
+
+    api_key = os.environ['canvas_api_key']
+
+    headers = {'Authorization' : 'Bearer {}'.format(api_key)}
+
+    courses = []
+
+    os.mkdir(os.path.join(Path.home(), 'canvas_documents'))
+
+    r = canvas.get(api_url + '/courses?per_page=1000', headers=headers)
+
+    for course in r.json():
+        try:
+            courses.append(Course(course['id'], course['course_code']))
+        except KeyError:
+            pass
+
+    for course in courses:
+        print('call dl')
+        course.download_files()
